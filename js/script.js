75ba266d1a23cf6ad5a75d200bb965ab58be539f
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-03-12 18:59:21 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let formElement = document.querySelector('.js-form');
+let amountElement = document.querySelector('.js-amount');
+let currencyElement = document.querySelector('.js-currency');
+let resultElement = document.querySelector('.js-result');
+
+let rateEUR = 4.19;
+let rateUSD = 3.84;
+
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case 'EUR':
+            result = (amount / rateEUR).toFixed(2);
+            break;
+
+        case 'USD':
+            result = (amount / rateUSD).toFixed(2);
+            break;
+    }
+
+    resultElement.innerHTML = `${amount} PLN = ${result} ${currency}`;
+
+});
